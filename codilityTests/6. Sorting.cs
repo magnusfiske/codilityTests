ebaa4ca227759d9f9936c067d7a8694b8c57408e
Magnus Fiske <94929311+magnusfiske@users.noreply.github.com>
2024-04-25 15:45:34 +0200

    Update 6. Sorting.cs
    added maxProductOfThree

--- codilityTests/6. Sorting.cs
+++ codilityTests/6. Sorting.cs
@@ -10,7 +10,7 @@ namespace codilityTests
     public class Sorting
     {
         //Distinct
-        public int solution(int[] A)
+        public int Solution(int[] A)
         {
             int n = A.Length;
             int result = 0;
@@ -30,6 +30,17 @@ namespace codilityTests
                 }
             }
             return result;
+        }
+
+        //MaxProductOfThree
+        public int Solution2(int[] A)
+        {
+            Array.Sort(A);
+        
+            var topMax = A[A.Length - 1] * A[A.Length - 2] * A[A.Length - 3];
+            var bottomMax = A[0] * A[1] * A[A.Length - 1];
+        
+            return Math.Max(topMax, bottomMax);
         }
     }
 }
